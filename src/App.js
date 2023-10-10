28c65e446617e8daede33f4d0622f848063ab676
Saif Rahman <Saf.saif@gmail.com>
2023-10-10 12:46:37 +0600

    darkmode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import SideBar from './SideBar';
 import Menu from './MenuBar';
 
 function App() {
+
+  
   return (
-    <div>
+    <div className='w-screen h-screen dark:bg-black dark:text-white'>
+      
       <SideBar className="flex"/>
       <Menu/>
+      <p>hello hell</p>
     </div>
   );
 }
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Menu() {
+
+  function toggleTheme() {
+    document.documentElement.classList.toggle("dark");
+  }
+
   return (
-    <header className='border-b border-green-950 py-2'>
+    <header className='border-b border-green-950 py-2 '>
       
+      <button onClick={toggleTheme} className='absolute right-10 top-2 bg-green-950 text-white px-5 py-0
+                          rounded hover:bg-green-900  '>Dark mode</button>
         <div className='flex items-center justify-center'>
             hell
         </div>
