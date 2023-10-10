3a1cc6f07eb6858645cbf1b50bcb6ad7ace1e56f
Saif Rahman <Saf.saif@gmail.com>
2023-10-10 18:21:56 +0600

    darkmode complete

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Menu() {
+
+  let [darkMode, setDarkMode] = useState(false);
+
+  useEffect (() => {
+    if (darkMode){
+      document.documentElement.classList.add("dark");
+    }else {
+      document.documentElement.classList.remove("dark");
+    }
+    
+  }, [darkMode])
 
   function toggleTheme() {
     document.documentElement.classList.toggle("dark");
@@ -9,8 +20,11 @@ function Menu() {
   return (
     <header className='border-b border-green-950 py-2 '>
       
-      <button onClick={toggleTheme} className='absolute right-10 top-2 bg-green-950 text-white px-5 py-0
-                          rounded hover:bg-green-900  '>Dark mode</button>
+      <button onClick={() => {
+            setDarkMode(!darkMode)
+      }} className='absolute right-10 top-2 bg-green-950 text-white px-5 py-0
+                          rounded hover:bg-green-900  '> 
+                          {darkMode ? "Light" : "Dark"} Mode</button>
         <div className='flex items-center justify-center'>
             hell
         </div>
